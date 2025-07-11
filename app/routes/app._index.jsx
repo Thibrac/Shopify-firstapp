4cada3316ada26cb27a62bcf6fb0b55207b6d847
traccurt <traccurt@student.42.fr>
2025-07-11 11:17:26 +0200

    Changement du message de bienvenue

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -118,31 +118,10 @@ export default function Index() {
               <BlockStack gap="500">
                 <BlockStack gap="200">
                   <Text as="h2" variant="headingMd">
-                    Congrats on creating a new Shopify app 🎉
+                    Custom Raffle Shopify app
                   </Text>
                   <Text variant="bodyMd" as="p">
-                    This embedded app template uses{" "}
-                    <Link
-                      url="https://shopify.dev/docs/apps/tools/app-bridge"
-                      target="_blank"
-                      removeUnderline
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    interface examples like an{" "}
-                    <Link url="/app/additional" removeUnderline>
-                      additional page in the app nav
-                    </Link>
-                    , as well as an{" "}
-                    <Link
-                      url="https://shopify.dev/docs/api/admin-graphql"
-                      target="_blank"
-                      removeUnderline
-                    >
-                      Admin GraphQL
-                    </Link>{" "}
-                    mutation demo, to provide a starting point for app
-                    development.
+                    This app is my first app: a raffle is a random draw to select one or more winners among registered participants.
                   </Text>
                 </BlockStack>
                 <BlockStack gap="200">
